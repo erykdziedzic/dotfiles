00497a0ab5a0d1e0e8b72efa6d0c751489a7f539
Eryk Dziedzic <eryk.marian.dziedzic@gmail.com>
2026-05-29 17:00:52 +0200

    feat: improve git aliases

--- shell/config.fish
+++ shell/config.fish
@@ -1,4 +1,8 @@
 fish_add_path /opt/homebrew/bin
 
 alias src="source ~/.config/fish/config.fish"
-abbr -a --set-cursor="%" gcm "git commit -am '%'"
+abbr -a --set-cursor gcm "git commit -m '%'"
+abbr -a --set-cursor gcam "git commit -am '%'"
+
+set -gx ANDROID_HOME $HOME/Library/Android/sdk
+set -gx PATH $PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator
