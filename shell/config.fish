ca9b706e58df14ce50ffe6bd3f278c37aceb2dea
Eryk Dziedzic <eryk.marian.dziedzic@gmail.com>
2026-08-15 12:58:39 +0200

    feat: record alias

--- shell/config.fish
+++ shell/config.fish
@@ -8,3 +8,4 @@ set -gx ANDROID_HOME $HOME/Library/Android/sdk
 set -gx PATH $PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator
 alias wispr "whisper --model tiny.en -f txt --language en --fp16 False"
 alias hush=~/projects/running/.claude/hooks/hush
+alias record 'ffmpeg -f avfoundation -i ":0" -af "pan=stereo|c0=c0+c2|c1=c1+c3" -c:a aac -b:a 128k mock.m4a'
